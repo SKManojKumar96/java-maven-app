34813fc386a9832ca7e8d9b9d1737119cf2f6bbc
Manoj Kumar <74124384+SKManojKumar96@users.noreply.github.com>
2026-02-10 19:31:32 +0530

    Update script.groovy syntax error credentials

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo 'building the image....'
-    withCredentials([usernamePassword:(credentialsId: 'dockerhub-id', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'USER', passwordVariable: 'PASS')]){
         sh 'docker build -t vetri1807/demo-app:jma-1.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push vetri1807/demo-app:jma-1.1'
